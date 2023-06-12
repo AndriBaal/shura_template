d4b5054fb98e5ca4280a71ec4552ca310263ec42
Andri <andri.baal@bluewin.ch>
2023-06-12 16:21:51 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,30 @@ use shura::*;
 fn shura_main(config: ShuraConfig) {
     // Starting your game with a new scene
     config.init(NewScene::new(1, |ctx| {
+        register!(ctx.components, [DummyComponent]);
         // Load the shared resources
-        ctx.insert_scene_state(DummyResources {
-            sprite: ctx.create_sprite(include_bytes!("../res/burger.png")),
+        ctx.scene_states.insert(DummyResources {
+            sprite: ctx.gpu.create_sprite(include_bytes!("../res/burger.png")),
         });
         // Set camera size
-        ctx.set_camera_scale(WorldCameraScale::Min(5.0));
+        ctx.world_camera.set_scaling(WorldCameraScale::Min(5.0));
         // Add your components to the default group
-        ctx.add_component(DummyComponent::new(Vector::new(3.0, 0.0), Vector::new(0.5, 0.5),  ctx));
-        ctx.add_component(DummyComponent::new(Vector::new(-3.0, 1.0), Vector::new(1.0, 1.0),  ctx));
+        ctx.components.add(DummyComponent::new(
+            Vector::new(3.0, 0.0),
+            Vector::new(0.5, 0.5),
+            ctx,
+        ));
+        ctx.components.add(DummyComponent::new(
+            Vector::new(-3.0, 1.0),
+            Vector::new(1.0, 1.0),
+            ctx,
+        ));
     }))
 }
 
 #[derive(State)]
 // Define a state that all components can access. In this case,
-// this is used for component independent resources. A state is always
+// this is used for comonent independent resources. A state is always
 // a singleton.
 struct DummyResources {
     // Sprite to render
@@ -34,14 +43,14 @@ struct DummyComponent {
     // Base of a component. Used to base information like the position
     // of the component or the rigid body if it has one
     #[base]
-    base: BaseComponent,
+    base: PositionComponent,
 }
 
 impl DummyComponent {
     pub fn new(translation: Vector<f32>, size: Vector<f32>, ctx: &Context) -> Self {
         DummyComponent {
-            model: ctx.create_model(ModelBuilder::cuboid(size)),
-            base: BaseComponent::new(PositionBuilder::default().translation(translation)),
+            model: ctx.gpu.create_model(ModelBuilder::cuboid(size)),
+            base: PositionComponent::new(PositionBuilder::default().translation(translation)),
         }
     }
 }
@@ -50,28 +59,23 @@ impl ComponentController for DummyComponent {
     const CONFIG: ComponentConfig = ComponentConfig {
         // call `update` every frame
         update: UpdateOperation::EveryFrame,
-        ..DEFAULT_CONFIG
+        ..ComponentConfig::DEFAULT
     };
-    fn update(active: &ActiveComponents<Self>, ctx: &mut Context) {
-        let frame_time = ctx.frame_time(); // or ctx.frame_manager.frame_time()
+    fn update(ctx: &mut Context) {
+        let frame_time = ctx.frame.frame_time();
 
         // Iterate over all active dummies
-        for dummy in ctx.active_mut(active) {
+        for dummy in ctx.components.iter_mut::<Self>() {
             // or ctx.component_manager.active_mut(active)
-            let new_rot = dummy.rotation().angle() + frame_time * -1.0;
-            dummy.set_rotation(Rotation::new(new_rot));
+            let new_rot = dummy.base.rotation().angle() + frame_time * -1.0;
+            dummy.base.set_rotation(Rotation::new(new_rot));
         }
     }
-    fn render(active: &ActiveComponents<Self>, ctx: &Context, encoder: &mut RenderEncoder) {
+    fn render(ctx: &Context, encoder: &mut RenderEncoder) {
         // Render each component with it's own Model but use the shared sprite from the state
-        let state = ctx.scene_state::<DummyResources>();
-        ctx.render_each(
-            active,
-            encoder,
-            RenderConfig::WORLD,
-            |renderer, dummy, instance| {
-                renderer.render_sprite(instance, &dummy.model, &state.sprite)
-            },
-        )
+        let state = ctx.scene_states.get::<DummyResources>();
+        encoder.render_each::<Self>(ctx, RenderConfig::WORLD, |renderer, dummy, instance| {
+            renderer.render_sprite(instance, &dummy.model, &state.sprite)
+        });
     }
 }
